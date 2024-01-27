d655a534bc9f0e0333dbc9ad12c989c213ab1881
Yaroslav <yaroslav.belyanski@yandex.ru>
2024-01-27 17:20:18 +0300

    Fix CSRF token

--- psychotype_joker.backend/psychotype_joker/settings.py
+++ psychotype_joker.backend/psychotype_joker/settings.py
@@ -14,6 +14,8 @@ DEBUG = os.getenv('DEBUG', False) == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '').split(',')
 
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', '').split(',')
+
 
 
 INSTALLED_APPS = [
