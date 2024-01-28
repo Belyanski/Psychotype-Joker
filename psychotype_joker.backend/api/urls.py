71b02edf03b20fc0c7d2bb36cb8f24f1aa01613e
Yaroslav <yaroslav.belyanski@yandex.ru>
2024-01-28 12:36:42 +0300

    Fix requirements

--- psychotype_joker.backend/api/urls.py
+++ psychotype_joker.backend/api/urls.py
@@ -4,8 +4,8 @@ from rest_framework.routers import DefaultRouter
 from .views import JokeViewSet, StorysViewSet
 
 router_v1 = DefaultRouter()
-router_v1.register(r'storys', StorysViewSet, basename='storys')
 router_v1.register(r'jokes', JokeViewSet, basename='jokes')
+router_v1.register(r'storys', StorysViewSet, basename='storys')
 
 urlpatterns = [
     path('', include(router_v1.urls)),
